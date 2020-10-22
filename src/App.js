7c6c3e2fec00556f5a14209e709d0b88243d0440
DucSkii <ducvietdao@live.co.uk>
2020-10-22 13:06:15 +0100

    added colour indicator, next change colour of numbers in history

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Form from './components/Form/Form'
 import Progress from './components/Progress/Progress'
 import { generateRandomNumber, absDiffCheck } from './utils'
 
+import './App.css'
+
 class App extends React.Component {
   state = {
     generatedNumber: generateRandomNumber(),
@@ -17,24 +19,26 @@ class App extends React.Component {
     show: false,
     numberColour: "#000000",
     numberFeedback: '',
+    numberFeedbackColour: '',
   }
 
   updateAppState = (guess) => {
     const { generatedNumber, attempt } = this.state
     const absDiff = Math.abs(guess - generatedNumber)
     console.log(generatedNumber, guess, absDiff)
-    const { numberColour, numberFeedback } = absDiffCheck(absDiff, attempt)
+    const { numberColour, numberFeedback, numberFeedbackColour } = absDiffCheck(absDiff, attempt)
     this.setState(prevState => ({
       guess,
       allGuesses: [ ...prevState.allGuesses, { guess } ],
       attempt: prevState.attempt + 1,
       numberFeedback,
+      numberFeedbackColour,
     }))
-
+    console.log(numberFeedbackColour)
   }
 
   render() {
-    const { allGuesses, attempt, numberFeedback } = this.state
+    const { allGuesses, attempt, numberFeedback, numberFeedbackColour } = this.state
     const guessList = allGuesses.map((item, index) => {
       return (
         <li key={index}>
@@ -50,7 +54,9 @@ class App extends React.Component {
             <Typography align="center" variant="h2" gutterBottom>HOT or COLD</Typography>
             <Divider style={{ margin: '20px 0' }} />
             <Form returnGuessToApp={guess => this.updateAppState(guess)} />
-            <div className="numberFeedback">{numberFeedback}</div>
+            <div className="numberFeedback-wrapper">
+              <div className={`numberFeedback-${numberFeedbackColour}`}>{numberFeedback}</div>
+            </div>
             <Refresh buttonName="Reset" />
             <HowTo />
             <Progress attempt={attempt} guessList={guessList} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,6 +4,7 @@ export const absDiffCheck = (absDiff, attempt) => {
 
   let numberColour
   let numberFeedback
+  let numberFeedbackColour
 
   const winAlert = () => {
     alert(`Congratulations you won with ${attempt} guesses!`)
@@ -16,21 +17,27 @@ export const absDiffCheck = (absDiff, attempt) => {
   } else if (absDiff < 6 && absDiff !== 0) {
     numberColour = "#ff0000"
     numberFeedback = "Very hot!"
+    numberFeedbackColour = '1'
   } else if (absDiff < 11 && absDiff > 5) {
     numberColour = "#f55d5d"
     numberFeedback = "Hot"
+    numberFeedbackColour = '2'
   } else if (absDiff < 16 && absDiff > 10) {
     numberColour = "#ed7445"
     numberFeedback = "Warm"
+    numberFeedbackColour = '3'
   } else if (absDiff < 26 && absDiff > 15) {
-    numberColour = "#eb4ed6"
+    numberColour = "#87a3df"
     numberFeedback = "Cold"
+    numberFeedbackColour = '4'
   } else if (absDiff > 25) {
-    numberColour = "#81d8f7"
+    numberColour = "#48afd4"
     numberFeedback = "Very cold"
+    numberFeedbackColour = '5'
   }
   return {
     numberColour,
     numberFeedback,
+    numberFeedbackColour,
   }
 }
